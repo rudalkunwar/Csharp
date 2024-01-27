fc18c646ca3ef08b14db4ef2ff0c67f6440c21e5
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-01-27 19:31:00 +0545

    jaggedArray

---
+++ jaggeedarray.cs
@@ -0,0 +1,10 @@
+using System;
+public class JaggedArray{
+    public static void Main(string[] args){
+        int[][] Jarr = new int [2][];
+        Console.WriteLine("Enter the values of jagged arrays");
+        for(int i = 0 ; i < 2 ; i++){
+            for(int j=0;j < )
+        }
+    }
+}
