03276916bdfe2d0b9b7fba4accbeffd0d53c13b7
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-02-23 19:56:01 +0545

    inheritance

--- inherit.cs
+++ inherit.cs
@@ -1,3 +1,22 @@
+using System;
 public class Vehicle{
-    
+    public string oil = "Petrol";
+}
+public class Bike:Vehicle{
+    string color;
+    public Bike(string color){
+        this.color = color;
+    }
+    public void display(){
+        Console.WriteLine("The vehicle type is "+oil);
+        Console.WriteLine("The color of bike is:"+color);
+    }
+}
+public class Test{
+    public static void Main(string[] args){
+        Bike b1 = new Bike("Blue");
+        b1.display();
+        Bike b2 = new Bike("Yellow");
+        b2.display();
+    }
 }
