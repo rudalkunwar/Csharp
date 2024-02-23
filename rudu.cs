fcfbc00dfdc69a30309da5e8c577c4f964b3cda4
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-02-23 20:10:54 +0545

    questions

--- rudu.cs
+++ rudu.cs
@@ -1,12 +1,134 @@
-using System;
-public class Test {
-public static void Main(string[] args){
+Tribhuvan University
+Faculty of Humanities & Social Sciences
+OFFICE OF THE DEAN
+2018
+Bachelor in Computer Application (BCA) 
+Course Title: MIS and E-Business
+Code No: CACS 301
+Semester: V
 
+Full Marks: 60 
+Pass Marks: 24 
+Time: 3 hours
 
-Console.WriteLine("HELLO rudal kunwar");
+Group B
+Attempt any SIX questions: [6*5=30]
+2. Explain C2C and B2B e-commerce models with example. [2.5+2.5]
+3. What is M-Commerce? Explain its benefits for organization, individual and society. [2+3]
+4. Define wireless communication. Differentiate between UMTS and LTE wireless data communication technologies. [1+4]
+5. Discuss how do enterprise applications are used in organization. [5]
+6. What is digital signature? Explain the requirements of digital signature in secure transaction. How does it differ from virtual currency? [2+1+2]
+7. Explain cryptocurrency with its legal provisions in Nepal. How is it different from virtual currency? Explain the importance of SEO for the growth of a website. [3+2]
 
-Console.WriteLine("HELLO rosa ");
-}
+Group C [2*10=20)
+Attempt any TWO questions:
 
+9. Discuss the security requirements of Internet and E-commerce applications. Explain how these requirements are fulfilled by various hardware and software systems. [10]
 
-}
+10. "The use of electronic payment systems has significantly increased in Nepal in recent years". In this context, explain the trend of cashless transaction in Nepal along with the benefits and downsides of digital payment systems and cashless economy. [10]
+
+Scanned with CamScanner
+
+Tribhuvan University
+Faculty of Humanities & Social Sciences
+OFFICE OF THE DEAN
+2018
+Full Marks: 60
+Pass Marks: 24
+Time: 3 hours
+
+Bachelor in Computer Application (BCA)
+Course Title: Computer Networking
+Code No: CACS 303
+Semester: V
+
+Candidates are required to answer the questions in their own words as far as possible.
+
+Group B
+Attempt any SIX questions: [6x5=30]
+1. Explain TCP/IP Reference model. [5]
+2. "Slotted ALOHA improves the performance of system over pure ALOHA". Justify. [5]
+3. Differentiate between Reliable & Unreliable connection with their application. [5]
+4. What is DNS? How does it work? [1+4]
+5. What is cryptography? Explain the principles of cryptography. [1+4]
+6. What is TCP? Describe TCP segment header. [1+4]
+7. Write a short note on (Any Two): [2.5+2.5]
+   a) Fragmentation
+   b) Channelization
+   c) VPN
+
+Group C
+Attempt any TWO questions: [2*10=20]
+1. Classify and calculate the total number of Networks and Hosts per network in IPV4. [10]
+2. Explain the IPV4 Header format in detail. [10]
+
+Scanned with CamScanner
+
+Tribhuvan University
+Faculty of Humanities & Social Sciences
+OFFICE OF THE DEAN
+2018
+Full Marks: 60
+Pass Marks: 24
+Time: 3 hours
+
+Bachelor in Computer Application (BCA)
+Course Title: Introduction to Management
+Code No: CAMG 304
+Semester: V
+
+Candidates are required to answer the questions in their own words as far as possible.
+
+Group B
+Attempt any SIX questions: [6x5=30]
+1. Define management? What kinds of skills required for a successful manager?
+2. Describe the major characteristics of contingency theory. Why is there "no one best way" to manage?
+3. What is planning? Explain the different levels of planning.
+4. Define organizational structure. What are the differences between centralization and decentralization?
+5. Define communication and explain the various steps in the communication process.
+6. What is leadership? Explain the different styles of leadership.
+7. What are the major techniques of control? Explain.
+
+Group C
+Attempt any Two questions: [2x10=20]
+1. Decision making is a very important managerial function. With the statement, discuss the meaning and process of decision making. Also, explain the types of decision making.
+2. "Quality improvement has no barriers and limitations". In light of this statement, explain the concept of Total Quality Management and explain different tools of TQM.
+3. Explain the concept of change in the organization. Why do employees resist change in the organization? And suggest various measures to overcome such resistance to change.
+
+Scanned with CamScanner
+
+Tribhuvan University
+Faculty of Humanities & Social Sciences
+OFFICE OF THE DEAN
+2018
+
+Full Marks: 60
+Pass Marks: 24
+Time: 3 hours
+
+Bachelor in Computer Application (BCA)
+Course Title: Computer Graphics and Animation
+Code No: CACS 305
+Semester: V
+
+Candidates are required to answer the questions in their own words as far as possible.
+
+Group B
+Attempt any six questions:
+
+1. What do you mean by area filling? Explain boundary fill algorithm.
+2. Define scaling transformation? Prove that two successive scaling are multiplicative.
+3. What is the symmetric property of a circle? Digitize a circle whose center is at origin and radius is 9.
+4. Define window and view port. Explain 2D viewing transformation pipeline.
+5. Reflect a prism A(0,0,0), B(1,1,0), C(1,2,2) and (0,2,0) about yz-plane which has been rotated previously with +90 degrees about y-axis.
+6. What do you mean by visible surface detection? Explain Z buffer algorithm for visible surface detection.
+7. What is animation? Explain the steps to create an animation sequence.
+
+Group C
+Attempt any two questions:
+
+1. Define refresh rate and persistence of monitor. Explain architecture of random scan display system with block diagram.
+2. How can you draw a line using Bresenham's line drawing algorithm? Trace it to rasterize the line with endpoints P (1, 4) and Q (7, 8).
+3. Explain Cohen Sutherland line clipping algorithm. Let ABCD be the rectangular window with A(20,20), B(90,20), C(90,70) and D(20,70). Use Cohen Sutherland algorithm to find the region codes and clip the lines with endpoints P(10,30) and Q(80,80).
+
+Scanned with CamScanner
