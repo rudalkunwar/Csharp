57d976131225975e93ef61ed1d3fc3d921d4a9ae
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-02-24 20:12:18 +0545

    the factorial using recursion

---
+++ recursion.cs
@@ -0,0 +1,20 @@
+using System;
+public class Recur{
+    public int recur(int x){
+        if(x==0){
+            return 1;
+        }else{
+            return x * recur(x-1);
+        }
+    }
+    public static void Main(string [] args){
+        int x;
+        Console.WriteLine("Enter the value");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        Recur r = new Recur();
+        int value = r.recur(x);
+        Console.WriteLine("The factorial value"+value);
+
+    }
+}
