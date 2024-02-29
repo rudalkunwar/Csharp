273d3799efc0b7b3daa99e85fd03889f38666cf8
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-02-29 19:24:56 +0545

    practical lets goo

--- labwork/practical10.cs
+++ labwork/practical10.cs
@@ -1,26 +1,13 @@
 using System;
 public class Practical{
     public static void Main(string[] args){
-        Console.WriteLine("Enter the length of Array");
-        int len = Convert.ToInt32(Console.ReadLine());
-        int[] num = new int[len];
-        Console.WriteLine("Enter the Values of Array");
-        for(int i = 0 ; i < len ; i++){
-            num[i] = Convert.ToInt32(Console.ReadLine());
-        }
-        for(int i = 0 ; i<len;i++){
-            for(int j = i+1 ; j<len ; j++){
-                if(num[i]>num[j]){
-                    int temp = num[i];
-                    num[i]=num[j];
-                    num[j]=temp;
-                }
-            }
-        }
-        Console.WriteLine("The sorted Array:");
-        foreach(int value in num){
-            Console.Write(value+" ");
-        }
-        
+        Console.WriteLine("Enter the two String");
+        string str1 = Console.ReadLine();
+        string str2 = Console.ReadLine();
+        Console.WriteLine("The length of the first string is "+str1.Length);
+        Console.WriteLine("The length of the Second string is "+str2.Length);
+        Console.WriteLine("The concatination of two string is : "+string.Concat(str1,str2));
+        Console.WriteLine(str1.Equals(str2)?"They are same string":"They are different string");
+        Console.WriteLine(str1.Contains("a")?"The first string conatins a":"The first string doesnot contain a");
     }
 }
