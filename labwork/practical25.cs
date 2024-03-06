cff4f1020844f9d5e90cf8e7c1203c049c78bac1
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-03-06 21:33:22 +0545

    visual

--- labwork/practical25.cs
+++ labwork/practical25.cs
@@ -1,6 +1,7 @@
 using System;
 publi class Practical{
     public static void Main(string[] args){
-        Console.WriteLine("I am gooing to do the lab 25 here");
+        Console.WriteLine("I am gooing to do the lab 25");
+        Console.WriteLine("This will be done in visual studio");
     }
 }
